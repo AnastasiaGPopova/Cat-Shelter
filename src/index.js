947f7a2db346ea4b850f85b30ecd68b37994357f
AnastasiaGPopova <ana.popova.register@gmail.com>
2023-01-25 15:34:33 +0200

    configured view Engine (handle bars)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const config = require('./config/configuration.js')
+const setViewEngine = require('./config/viewEngineConfig.js')
 
 const app = express()
+
+setViewEngine(app)
+
+app.use(express.static('src/public'))
+
+
+
 app.get("/", (req,res) => {
-    res.send("Hello")
+    res.render("home", {layout: false})
+})
+
+app.get("/add-breed", (req,res) => {
+    res.render("add2", {layout: false})
 })
 
 app.listen(config.PORT, () => console.log(`Server is listening on Port ${config.PORT}`))
